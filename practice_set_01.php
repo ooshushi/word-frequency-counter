ede94c32603ad62270a5f014ee0627ecafe1537d
ooshushi <zajpanes@gmail.com>
2023-09-29 15:24:54 +0800

    Update practice_set_01.php

--- practice_set_01.php
+++ practice_set_01.php
@@ -1,38 +1,59 @@
 <?php
-// Poorly organized and hard-to-read code
-// Calculate the total price of items in a shopping cart
+/**
+ * Calculates the total price of items in a shopping cart.
+ *
+ * @param array $items An array of items with 'name' and 'price' keys.
+ * @return float Total price of the items
+ */
 $items = [
  ['name' => 'Widget A', 'price' => 10],
  ['name' => 'Widget B', 'price' => 15],
  ['name' => 'Widget C', 'price' => 20],
 ];
-$total = 0;
-foreach ($items as $item) {
- $total += $item['price'];
+
+function calcTotalPrice(array $items): float {
+    $total = 0;
+    foreach ($items as $item) {
+        $total += $item['price'];
+    }
+    return $total;
 }
-echo "Total price: $" . $total;
-// Perform a series of string manipulations
-●
-●
-●
-●
-●
-●
-1.
-○
-2.
-○
-$string = "This is a poorly written program with little
-structure and readability.";
-// Remove spaces and convert to lowercase
-$string = str_replace(' ', '', $string);
-$string = strtolower($string);
-// Display the modified string
-echo "\nModified string: " . $string;
-// Check if a number is even or odd
+
+$totalPrice = calcTotalPrice($items);
+            echo "Total price: $ $totalPrice";
+
+
+/**
+ * Modifies a string by removing spaces and converting it to lowercase.
+ *
+ * @param string $inputString The input string to be modified.
+ * @return string The modified string.
+ */
+
+ function modifiedString(string $inputString): string {
+    $modifiedString = str_replace(' ', '', $inputString);
+    $modifiedString = strtolower($modifiedString);
+    return $modifiedString;
+}
+$string = "This is a poorly written program with little structure and readability.";
+$newString = modifiedString($string);
+echo "Modified string: $newString";
+
+/**
+ * Checks if a number is even or odd.
+ *
+ * @param int $number The number to be checked.
+ * @return string A message indicating whether the number is even or odd.
+ */
+function EvenOrOdd(int $number): string {
+    if ($number % 2 == 0) {
+        return "The number $number is even.";
+    } else {
+        return "The number $number is odd.";
+    }
+}
+
 $number = 42;
-if ($number % 2 == 0) {
- echo "\nThe number " . $number . " is even.";
-} else {
- echo "\nThe number " . $number . " is odd.";
-}
+$message = EvenOrOdd($number);
+echo $message;
+?>
